5d1f918961a76c41ee9a0bb4b7dbaa0c6abd7480
hanerach <vkruglov1337@yandex.ru>
2024-02-23 00:44:46 +0300

    3

--- main.py
+++ main.py
@@ -18,5 +18,30 @@ def training(prof):
     return render_template('training.html', picture=pic)
 
 
+@app.route('/list_prof/<numerated>')
+def list_prof(numerated):
+    li = ["инженер-исследователь",
+          "пилот",
+          "экзобиолог",
+          "врач",
+          "инженер по терраформированию",
+          "климатолог",
+          "специалист по радиационной защите",
+          "астрогеолог",
+          "гляциолог",
+          "инженер жизнеобеспечения",
+          "метеоролог",
+          "оператор марсохода",
+          "киберинженер",
+          "штурман",
+          "пилот дронов"]
+    params = {
+        'professions': li,
+        'num': numerated
+    }
+    return render_template('list_prof.html', **params)
+
+
+
 if __name__ == '__main__':
     app.run(port=8001, host='127.0.0.1')
