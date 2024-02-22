8a686de9e35523aa679cbdc17162109724a13a03
hanerach <vkruglov1337@yandex.ru>
2024-02-23 00:05:42 +0300

    2

--- main.py
+++ main.py
@@ -9,5 +9,14 @@ def index(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    if 'инженер' in prof.lower() or 'строитель' in prof.lower():
+        pic = "../static/img/station_ns.jpg"
+    else:
+        pic = "../static/img/station_it.jpg"
+    return render_template('training.html', picture=pic)
+
+
 if __name__ == '__main__':
     app.run(port=8001, host='127.0.0.1')
