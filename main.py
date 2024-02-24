94ff72e6ffe49e74577af32e65578db18534fb3d
hanerach <vkruglov1337@yandex.ru>
2024-02-24 22:48:38 +0300

    5

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class LoginForm(FlaskForm):
+    id_astro = StringField('id астронавта', validators=[DataRequired()])
+    pw_astro = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    id_cap = StringField('id капитана', validators=[DataRequired()])
+    pw_cap = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Войти')
 
 
 @app.route('/<title>')
@@ -56,6 +68,14 @@ def answer():
         'ready': True
     }
     return render_template('auto_answer.html', **params)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Аварийный доступ', form=form)
 
 
 if __name__ == '__main__':
